25971b90066f5a299c5380cf836f4cd5d0a82737
Andre Luiz <andre14luiz21@gmail.com>
2024-09-18 19:35:27 -0300

    feat/*

---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import fastify from "fastify";
+import crypto from "node:crypto";
+import { knex } from "./database";
+
+const app = fastify();
+
+app.get("/hello", async () => {
+  const transaction = await knex("transactions").select("*");
+  return transaction;
+});
+
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => console.log("Server is running on port 3333"));
